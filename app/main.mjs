ae5393e347345c0b29c98fb8d18c690e0b017e4f
Remy Sharp <remy@remysharp.com>
2023-08-18 22:06:26 +0100

    simple telegram + rules

---
+++ app/main.mjs
@@ -0,0 +1,100 @@
+import "renvy";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import * as mqtt from "mqtt"; // import everything inside the mqtt module and give it the namespace "mqtt"
+
+const channelId = process.env.BOT_CHANNEL_ID;
+const botTopic = "state/bot";
+const devices = new Map();
+
+let client = mqtt.connect(`mqtt://${process.env.MQTT_HOST}`, {
+  username: process.env.MQTT_USER,
+  password: process.env.MQTT_PASS,
+  will: {
+    retain: true,
+    topic: botTopic,
+    payload: JSON.stringify("DISCONNECTED"),
+  },
+}); // create a client
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+client.on("connect", async () => {
+  console.log("[mqtt] connected");
+
+  client.publish(botTopic, JSON.stringify("CONNECTED"), {
+    retain: true,
+  });
+
+  ["state/+/kettle", "tasmota/discovery/#"].forEach((topic) => {
+    client.subscribe(topic, (err) =>
+      err
+        ? console.log(`[mqtt] failed to connect to ${topic}`, err)
+        : console.log(`[mqtt] connected to ${topic}`)
+    );
+  });
+
+  bot.telegram.sendMessage(channelId, "Bot has connected to mqtt");
+});
+
+client.on("message", (topic, message) => {
+  if (topic.startsWith("tasmota/discovery/") && topic.endsWith("/config")) {
+    // message is Buffer
+    const data = JSON.parse(message.toString());
+    console.log(`[mqtt] discovered device: ${data.t} / ${data.fn[0]}`);
+    // collect for mapping
+    devices.set(data.t, data.fn[0]);
+    bot.telegram.sendMessage(channelId, `Discovered "${data.fn[0]}" device`);
+  }
+
+  if (topic.startsWith("state/")) {
+    // this is the kettle or an error
+    const [, device, mode] = topic.split("/");
+    const name = devices.get(device);
+
+    if (!name) {
+      console.log(`unknown topic delivery`, { topic });
+      client.publish(
+        botTopic + "/log",
+        JSON.stringify({
+          error: "unknown topic",
+          topic,
+          device,
+          message: message.toString(),
+        })
+      );
+    }
+
+    const value = parseInt(message.toString(), 10);
+    console.log({ topic, message, device, mode, value });
+    if (value === 1) {
+      console.log("sending");
+      bot.telegram.sendMessage(channelId, `${name} is boiling the kettle`);
+    }
+    if (value === -1) {
+      // means the timer has expired and we expected them to have boiled
+      bot.telegram.sendMessage(channelId, `⚠️ ${name} hasn't checked in`);
+    }
+  }
+});
+
+bot.command("quit", async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.leaveChat(ctx.message.chat.id);
+
+  // Using context shortcut
+  await ctx.leaveChat();
+});
+
+bot.on(message("text"), async (ctx) => {
+  // Explicit usage
+  console.log(ctx);
+  // Using context shortcut
+  await ctx.reply(`Hello ${ctx.update.message.chat.first_name}`);
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
